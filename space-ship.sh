05141e2e569078ecc99b05584cda7b3e7d90d494
Maaacs <msb@icomp.ufam.edu.br>
2024-02-25 15:01:24 -0400

    separeted installation

--- space-ship.sh
+++ space-ship.sh
@@ -4,48 +4,7 @@
 # https://github.com/Maaacs/Spaceship-Dracula-Colors
 #
 
-# Checks SO 
-OS=$(uname -s)
-if [[ "$OS" == "Linux" ]]; then
-  echo "Installating dependences on Linux..."
-  #sudo rm /var/lib/dpkg/info/format
-  #sudo printf "1\n" > /var/lib/dpkg/info/format
-  sudo add-apt-repository universe
-  sudo apt update && sudo apt upgrade -y
-  sudo apt-get install git-all
-  #sudo apt install zsh
-  #chsh -s $(which zsh)
-  #touch ~/.zshrc
-  sudo apt install fonts-firacode
-  echo "Finish install dependences on Linux."
-elif [[ "$OS" == "Darwin" ]]; then
-  echo "Start installation on MacOS X."
-  # Verify brew installation
-  if ! brew --version > /dev/null 2>&1; then
-    echo "Brew not found. Installing brew...!"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || { echo "Failed to install Brew."; exit 1; }
-  fi
-  # Brew is guaranteed to be installed
-  if ! brew list gnu-sed > /dev/null 2>&1; then
-    brew install gnu-sed
-  fi
-  # Check and append to .zshrc only once
-  if [[ -f ~/.zshrc && -w ~/.zshrc && ! $(grep 'GNUD-SED' ~/.zshrc) ]]; then
-    echo '
-#START GNUD-SED
-PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-#END GNUD-SED
-' >> ~/.zshrc
-    echo "Gnu-sed installed and configured."
-  else
-    echo "Gnu-sed already installed or .zshrc not found/not writable."
-  fi
-else
-  echo "Unsupported operating system."
-fi
-
 if [[ -f ~/.zshrc && -w ~/.zshrc ]]; then
-    sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
     echo "Istalling zdharma..."
     echo '#START HIGHLIGHTING AUTOSUGGESTIONS COMPLETIONS' >> ~/.zshrc
     bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
