d6f8e900e33492d0070997a27cc9dfe283521279
Maaacs <msb@icomp.ufam.edu.br>
2024-02-25 16:12:05 -0400

    separeted installation

--- dependences.sh
+++ dependences.sh
@@ -15,6 +15,20 @@ if [[ "$OS" == "Linux" ]]; then
   echo "Finish install dependences on Linux."
   echo "Installing ohmyzsh"
   sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+  fonts_dir="${HOME}/.local/share/fonts"
+  if [ ! -d "${fonts_dir}" ]; then
+    echo "mkdir -p $fonts_dir"
+    mkdir -p "${fonts_dir}"
+  else
+    echo "Found fonts dir $fonts_dir"
+  fi
+  version=5.2
+  zip=Fira_Code_v${version}.zip
+  curl --fail --location --show-error https://github.com/tonsky/FiraCode/releases/download/${version}/${zip} --output ${zip}
+  unzip -o -q -d ${fonts_dir} ${zip}
+  rm ${zip}
+  echo "fc-cache -f"
+  fc-cache -f
   echo "Finish ohmyzsh install"
 elif [[ "$OS" == "Darwin" ]]; then
   echo "Start installation on MacOS X."
